3d5c5d13ab8fe3ff821808db7b8981036b4c6a57
Alexandre Ozelo <alexandre.ozelo@sensedia.com>
2018-05-28 09:18:27 -0300

    First commit

---
+++ src/main/java/com/sensedia/userbets/UserBetsApplication.java
@@ -0,0 +1,12 @@
+package com.sensedia.userbets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserBetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserBetsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sensedia/userbets/UserBetsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sensedia.userbets;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserBetsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
